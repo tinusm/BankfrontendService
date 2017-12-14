5ca9ed0e97114e8249181321d60aec6bdea2b049
smtinu <tinusm1980@gmail.com>
2017-12-14 13:24:41 +0530

    test

---
+++ BankfrontendService/src/main/java/com/micro/micro/StartUpClass.java
@@ -0,0 +1,12 @@
+package com.micro.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUpClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUpClass.class, args);
+	}
+}
